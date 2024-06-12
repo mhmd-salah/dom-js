39d541ff05640f437b9fee33a4174163a2a972d2
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-06-12 14:33:40 +0200

    add data type interface

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//hello world 
+// https://developer.mozilla.org/en-US/docs/Glossary/HTML 
+/*
+- data type interface
+  Document
+  Node
+  Element
+  NodeList
+  Attr
+  NamedNodeMap
+*/
+
+console.log(document.childNodes[1])
