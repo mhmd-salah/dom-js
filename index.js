9ed9541cfc52e24afabcbb0436610c49c78cb644
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-06-12 22:10:16 +0200

    pending

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// https://developer.mozilla.org/en-US/docs/Glossary/HTML 
+// https://developer.mozilla.org/en-US/docs/Glossary/HTML
 /*
 - data type interface
   Document
@@ -10,11 +10,16 @@
 */
 // get attributes element  => element.attributes["att name"].value
 
-console.log(document.childNodes[1])
-console.log(document.COMMENT_NODE)
-console.log(document.childNodes)
-console.log(document.Attr)
-let parent = document.querySelector(".parent")
-console.log(parent.attributes["data-par"].value)
-let hello = "mohamed salah";
+// console.log(document.childNodes[1])
+// console.log(document.COMMENT_NODE)
+// console.log(document.childNodes)
+// console.log(document.Attr)
+// let parent = document.querySelector(".parent")
+// console.log(parent.attributes["data-par"].value)
+// let hello = "mohamed salah";
 
+// console.log("hello")
+document.querySelector(".child").attributes[0].value = "first child";
+
+console.log([...document.querySelector(".child").attributes]);
+console.log(document.documentElement);
